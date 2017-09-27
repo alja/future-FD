125a990a147550febb23b195ffa18412d90d0d4d
Alja Mrak-Tadel <amraktadel@ucsd.edu>
2017-09-27 15:18:02 -0700

    dbg

--- openui5/Detail.controller.js
+++ openui5/Detail.controller.js
@@ -7,7 +7,20 @@ sap.ui.define([
 	return Controller.extend("flexiblecolumnlayout.Detail", {
 		onInit: function () {
 			this.oRouter = this.getOwnerComponent().getRouter();
-			this.oModel = this.getOwnerComponent().getModel();
+		    this.oModel = this.getOwnerComponent().getModel();
+		    console.log("set tank model");
+/*
+			var oModel3 = new sap.ui.model.json.JSONModel({ 
+				filename: "http://xrd-cache-1.t2.ucsd.edu/alja/tank.root", 
+				itemname: "geom", 
+				opt: "z" 
+			});
+		    var pp = sap.ui.getCore().byId("__xmlview2--panel3");
+		    console.log("whole ", pp);
+		    console.log("ID whole ", pp0);
+		    
+	            pp.setModel(oModel3, "__xmlview2--panel3");
+*/		    
 		},
 		handleDetailPress: function () {
 			var oNextUIState = this.getOwnerComponent().getHelper().getNextUIState(2);
